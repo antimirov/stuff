ff5ba1c936c4647dcea875c7538c41ee1ab1b685
Eugene Antimirov <turist@gmail.com>
2009-04-27 00:59:48 +0200

    Initial commit of shaper.py

---
+++ shaper.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import time
+import threading
+
+
+
+class Shaper:
+    def __init__(self, file, speed):
+        self.file = file
+        self.speed = speed
+        self.queue = []
+        self.curtime = round(time.time())
+        self.curcount = 0
+
+    def read(num=None):
+        #while round(time.time()) == self.curtime:
+        #while count < num:
+        res = self.file.read(num)
+        read_bytes = len(res)
+        self.count += read_bytes
+        if self.count <= self.speed:
+            if round(time.time()) == self.curtime:
+                return res
+        else:
+            count = 0
+            while 
+        
+
+
+
+def test_read(fr0m, to, speed):
+    s = Shaper(fr0m, speed)
+    while True:
+        buf = s.read(1024)
+        if buf:
+            to.write(buf)
+        else:
+            return
+
+
+
+
+if __name__ == "__main__":
+    f = open("from.dat", "r")
+    t = open("to.dat", "w")
+
+    test_read(f,t,32*1024)
+
