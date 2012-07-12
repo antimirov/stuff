8147baffb94cf9ddc0f0dadd290f0d8ecc2ac39e
Eugene Antimirov <turist@gmail.com>
2012-07-12 17:53:45 +0200

    Added task 17

---
+++ euler/17.py
@@ -0,0 +1,47 @@
+import sys
+
+
+d = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'fourty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'one hundred',
+    1000: 'one thousand'
+}
+
+
+def num2words(n):
+    return 123
+    
+
+
+def main():
+    print num2words(123)
+
+
+if __name__ == '__main__':
+    main()
